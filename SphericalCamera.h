a9289e1b30aa967f709b7a4d656a0ffc35a51e2e
Lyatus (Box) <lutopia@hotmail.fr>
2015-05-07 18:37:11 +0200

    Added center and radius getters to SphericalCamera

--- SphericalCamera.h
+++ SphericalCamera.h
@@ -14,6 +14,10 @@ class SphericalCamera : public L::GL::Camera {
     void addPoint(const L::Point3f&);
     void update(float deltaTime);
     void event(const L::Window::Event&);
+
+    inline const L::Point3f& center() const {return _center;}
+    inline float radius() const {return _center.dist(position());}
+
     static void configure();
 };
 
