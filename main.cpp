cb0adacbfc509a808f3d78dc938eea5399d2a50d
Lyatus (Box) <lutopia@hotmail.fr>
2015-05-30 16:06:42 +0200

    Changed budding curve for budding max distance

--- main.cpp
+++ main.cpp
@@ -45,10 +45,10 @@ float irrigationSphereRadius;
 float growthVPS, growthDuration;
 float thirstVPS, thirstAppearanceFactor;
 float chemoVPS, chemoPropagationFactor, chemoOrganFactor, chemoDuration, chemoDisappearanceFactor;
-float buddingVPS, buddingDuration, buddingMinY, buddingCount;
+float buddingVPS, buddingDuration, buddingMinY, buddingCount, buddingMaxDistance;
 float vesselCount, burstRadius;
 float buddingFactor, vesselBuddingFactor, chemoBuddingFactor;
-float buddingCurve, chemoBuddingCurve;
+float chemoBuddingCurve;
 bool anywhere(false), budding(true);
 Automaton* thirstAutomatonP;
 
@@ -200,7 +200,7 @@ void search() {
               }
               if(camPotential && (voxel.type()==Voxel::TUMOR || voxel.type()==Voxel::TUMOR_IDLE || voxel.type()==Voxel::TUMOR_THIRSTY || voxel.type()==Voxel::TUMOR_THIRSTY_IDLE))
                 cam.addPoint(chunk->position()+Point3i(x,y,z));
-              if(budPotential && voxel.type()==Voxel::TUMOR_IDLE && position.y()>buddingMinY && !Automaton::has(growth,(int)buddingCount) && Rand::nextFloat()<buddingFactor*(buddingCurve/(Bonus::distanceToInactive(position)+buddingCurve)) && !Automaton::has(growth,position))
+              if(budPotential && voxel.type()==Voxel::TUMOR_IDLE && position.y()>buddingMinY && !Automaton::has(growth,(int)buddingCount) && Bonus::distanceToInactive(position)<buddingMaxDistance && Rand::nextFloat()<buddingFactor && !Automaton::has(growth,position))
                 startTumor(position,buddingVPS,Time(buddingDuration*1000000.f));
               if(vesselBudPotential && (voxel.type()==Voxel::TUMOR_THIRSTY || voxel.type()==Voxel::TUMOR_THIRSTY_IDLE) && Rand::nextFloat()<vesselBuddingFactor)
                 sca.addTarget(position);
@@ -503,7 +503,7 @@ int main(int argc, char* argv[]) {
   Game::registerValue("budding_vps",&buddingVPS);
   Game::registerValue("budding_duration",&buddingDuration);
   Game::registerValue("budding_factor",&buddingFactor);
-  Game::registerValue("budding_curve",&buddingCurve);
+  Game::registerValue("budding_max_distance",&buddingMaxDistance);
   Game::registerValue("budding_min_y",&buddingMinY);
   Game::registerValue("budding_count",&buddingCount);
   Game::registerValue("vessel_budding_factor",&vesselBuddingFactor);
