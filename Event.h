42516e8228de2fc37761fe6b15c9a15412ec7b3d
Lyatus (Box) <lutopia@hotmail.fr>
2015-06-04 14:06:19 +0200

    Added tumor and vessel to event system

--- Event.h
+++ Event.h
@@ -2,14 +2,19 @@
 #define DEF_Cancer_Event
 
 #include <L/L.h>
+#include "SCA.h"
+#include "World.h"
 
 class Event {
   private:
     static L::Array<Event> _events;
-    typedef enum {ADD,MULT} Operation;
+    static World* _world;
+    static SCA* _sca;
+    typedef enum {ADD,MULT,VESSEL,TUMOR} Operation;
     float* _value;
     Operation _operation;
     float _parameter;
+    L::Point3f _position;
     L::Time _time;
 
   public:
@@ -17,7 +22,7 @@ class Event {
     Event(const L::Dynamic::Var&);
     bool update(const L::Time&);
     static void updateAll(const L::Time&);
-    static void configure();
+    static void configure(World*, SCA*);
 };
 
 #endif
---
+++ main.h
@@ -0,0 +1,8 @@
+#ifndef DEF_Cancer_Main
+#define DEF_Cancer_Main
+
+#include <L/L.h>
+
+void startGrowth(const L::Point3f&);
+
+#endif
